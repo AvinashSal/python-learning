fae430ca66b9569709f499ca7ed541d6115b1889
Avinash Salgar <avinashnsalgar@coep.sveri.ac.in>
2026-05-21 09:43:53 +0530

    Day1 file

---
+++ Day1_variable.py
@@ -0,0 +1,9 @@
+name="Avinash" #variable name string
+Roll_NO=62      # variable intiger 
+print(name,Roll_NO)
+
+#Addition of two numbers,sub,Mul,Div
+
+num1=45 
+num2=18
+print(num1+num2,num1-num2,num1*num2,num1/num2)
