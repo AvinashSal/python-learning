b6be7d44973f0456ba712d15beaf475ecf0a0f52
Avinash Salgar <avinashnsalgar@coep.sveri.ac.in>
2026-05-22 13:46:33 +0530

    multiplication simple project completed

---
+++ simple_projects/Day2_project.py
@@ -0,0 +1,5 @@
+#making multiplication project that take tabel input from user
+number=int(input("enter number for table : "))
+
+for i in range(1,11):                    #range start 1 to 10
+    print(f"{number}*{i}={number*i}")
