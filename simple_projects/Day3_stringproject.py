cbec8f69442d1e5553a76a5ed30ae3901043441b
Avinash Salgar <avinashnsalgar@coep.sveri.ac.in>
2026-05-23 11:07:04 +0530

    Added upper string feature fixes #2

---
+++ simple_projects/Day3_stringproject.py
@@ -0,0 +1,9 @@
+#making string utility tool
+text=input("enter a string : ")
+
+#taking user choice perform selected task
+choice=int(input("enter a choice : "))
+
+if choice==1 :
+    print(text.upper())
+    
