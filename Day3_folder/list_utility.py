0159be474f071d2bec050bb3e0af89b1e258375a
Avinash Salgar <avinashnsalgar@coep.sveri.ac.in>
2026-05-29 12:57:41 +0530

    Added loop functionality bug fixes #12

--- Day3_folder/list_utility.py
+++ Day3_folder/list_utility.py
@@ -4,35 +4,51 @@ animals=["cat","dog","deer","lion"]        #this is animals list
 
 print(animals)                             #printing for user to see it
 
-print("operation can perform on list : ")
-print("1.Add an animal ")
-print("2.Remove an animal ")
-print("3.Sort an animal list ")
-print("4.Reverse an animal list")
-print("5.")
-
-choice=int(input("enter your choice : "))   #taking choice from user
-
-if choice==1 :
-    add_anim=input("enter an animal to add : ")
-    print(animals)
-    position=int(input("enter the position to add :"))     #user enter the position
-    animals.insert(position-1,add_anim)                   #not an index position so {position-1}
-    print(animals)
-
-elif choice==2 :
-    print(animals)
-    remove_anim=int(input("enter the position of animal to remove :"))
-    # p=animals[remove_anim-1]
-    animals.remove(animals[remove_anim-1])     #user enter positon is +1 than index so {remove_anim-1}
-    print(animals)
-
-elif choice==3 :
-    print(animals)
-    animals.sort()
-    print(animals)
+while True :
     
-elif choice==4 :
-    print(animals)
-    animals.reverse()
-    print(animals)
+    print("operation can perform on list : ")
+    print("1.Add an animal ")
+    print("2.Remove an animal ")
+    print("3.Sort an animal list ")
+    print("4.Reverse an animal list")
+    print("5.exit")
+
+    choice=int(input("enter your choice : "))   #taking choice from user
+
+    if choice==1 :
+        add_anim=input("enter an animal to add : ")
+        print(animals)
+        position=int(input("enter the position to add :"))     #user enter the position
+        animals.insert(position-1,add_anim)                   #not an index position so {position-1}
+        print(animals)
+
+    elif choice==2 :
+        print(animals)
+        remove_anim=int(input("enter the position of animal to remove :"))
+        # p=animals[remove_anim-1]
+        animals.remove(animals[remove_anim-1])     #user enter positon is +1 than index so {remove_anim-1}
+        print(animals)
+
+    elif choice==3 :
+        print(animals)
+        animals.sort()
+        print(animals)
+        
+    elif choice==4 :
+        print(animals)
+        animals.reverse()
+        print(animals)
+        
+    elif choice==5 :
+        print("List is closed")
+        break
+    
+    else :
+        print("wrong choice entered")
+
+
+
+
+
+
+    
